75cdb473ed2c98132b00f14a9efba60a11a3559a
seersol92 <hamad.pixiders@gmail.com>
2018-04-23 18:30:25 +0500

    socket port

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -23,7 +23,7 @@ export class ChatService {
 
   connect(username: string, callback: Function = () =>  {}): void {
     // initialize the connection
-    this.socket = io('http://localhost:3000', { transports: ['websocket', 'xhr-polling'] });
+    this.socket = io(this.serverUrl, { transports: ['websocket', 'xhr-polling'] });
     this.socket.on('connect', () => {
       this.sendUser(username);
       console.log('connected to the chat server');
